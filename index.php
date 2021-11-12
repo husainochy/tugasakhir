f2f9bffbc47f6e62b120699f0f411bea2a193b15
Husain <ochy22@gmail.com>
2021-11-12 16:52:57 +0800

    no message

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+require "config.php";
+$sql = "select produk.*, categori.nm_categori, jenis_produk.nm_jenisproduk  
+        from produk, categori, jenis_produk 
+        where 
+        produk.id_categori=categori.id_categori
+        and
+        produk.id_jenisproduk=jenis_produk.id_jenisproduk
+        ";
+$query = mysqli_query($conn, $sql);
+?>
 <!DOCTYPE html>
 <!--[if IE 8]> <html class="ie8 oldie" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
@@ -17,8 +28,17 @@
 			<a href="index.html" id="logo" title="Mutiara Lombok">Mutiara Lombok</a>
 			<div class="right-links">
 				<ul>
+					<p>Time: <span id="tanggalwaktu"></span></p>
+					<script>
+					var dt = new Date();
+					document.getElementById("tanggalwaktu").innerHTML = (("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (("0"+dt.getDate()).slice(-2)) +"/"+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()+1).slice(-2)) +":"+ (("0"+dt.getMinutes()+1).slice(-2));
+					</script>
+				</ul>
+
+				<ul>
 					<li><a href="login.php"><span class="ico-account"></span>Login</a></li>
 				</ul>
+
 			</div>
 		</div>
 		<!-- / container -->
@@ -66,63 +86,21 @@
 			<div class="last-products">
 				<h2>Last added products</h2>
 				<section class="products">
-					<article>
-						<img src="images/1.jpg" alt="">
-						<h3>Excepteur sint occaecat</h3>
-						<h4>Rp. 250.000</h4>
-						<a href="cart.html" class="btn-add">Add to cart</a>
-					</article>
-					<article>
-						<img src="images/11.jpg" alt="">
-						<h3>Lorem ipsum dolor</h3>
-						<h4>Rp. 300.000</h4>
-						<a href="cart.html" class="btn-add">Add to cart</a>
-					</article>
-					<article>
-						<img src="images/12.jpg" alt="">
-						<h3>cupidatat non proident</h3>
-						<h4>Rp. 500.000</h4>
-						<a href="cart.html" class="btn-add">Add to cart</a>
-					</article>
-					<article>
-						<img src="images/13.jpg" alt="">
-						<h3>Duis aute irure</h3>
-						<h4>Rp. 650.000</h4>
-						<a href="cart.html" class="btn-add">Add to cart</a>
-					</article>
-					<article>
-						<img src="images/14.jpg" alt="">
-						<h3>magna aliqua</h3>
-						<h4>Rp 525.000</h4>
-						<a href="cart.html" class="btn-add">Add to cart</a>
-					</article>
-				</section>
-			</div>
-			<section class="quick-links">
-				<article style="background-image: url(images/2.jpg)">
-					<a href="#" class="table">
-						<div class="cell">
-							<div class="text">
-								<h4>Lorem ipsum</h4>
-								<hr>
-								<h3>Dolor sit amet</h3>
-							</div>
-						</div>
-					</a>
-				</article>
-				<article class="red" style="background-image: url(images/3.jpg)">
-					<a href="#" class="table">
-						<div class="cell">
-							<div class="text">
-								<h4>consequatur</h4>
-								<hr>
-								<h3>voluptatem</h3>
-								<hr>
-								<p>Accusantium</p>
-							</div>
-						</div>
-					</a>
-				</article>
+				<?php
+        $no = 1;
+while($isi = mysqli_fetch_object($query)){
+        ?>
+        <article>
+			<img src="<?= "file/". $isi->poto ?>" alt="">
+			<h3><?=$isi->nm_categori;?></h3>
+			<h4><?=$isi->deskripsi?></h4>
+			<h5><?=$isi->harga?></h5>
+			<a href="cart.html" class="btn-add">Add to cart</a>
+			</article>
+          
+        <?php } ?>
+				
+				
 				<article style="background-image: url(images/4.jpg)">
 					<a href="#" class="table">
 						<div class="cell">

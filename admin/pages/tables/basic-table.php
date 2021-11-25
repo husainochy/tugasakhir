14c92771821ced7647790c57f26e31654c07e1e7
Husain <ochy22@gmail.com>
2021-11-25 09:35:14 +0800

    no message

--- admin/pages/tables/basic-table.php
+++ admin/pages/tables/basic-table.php
@@ -84,12 +84,12 @@
             <li class="nav-item nav-profile">
               <a href="#" class="nav-link">
                 <div class="nav-profile-image">
-                  <img src="assets/images/faces/face1.jpg" alt="profile">
+                  <img src="../../assets/images/faces/face1.jpg" alt="profile">
                   <span class="login-status online"></span>
                   <!--change to offline or busy as needed-->
                 </div>
                 <div class="nav-profile-text d-flex flex-column">
-                  <span class="font-weight-bold mb-2"><?= $userdata->username; ?></span>
+                  <span class="font-weight-bold mb-2"><?= $_SESSION['username']; ?></span>
                   <span class="text-secondary text-small">Admin Toko</span>
                 </div>
                 <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
@@ -102,16 +102,16 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="pages/tables/basic-table.php">
+              <a class="nav-link" href="basic-table.php">
                 <span class="menu-title">Data Produk</span>
                 <i class="mdi mdi-table-large menu-icon"></i>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="../forms/basic_elements.php">
+            <a class="nav-link" href="../../pages/forms/basic_elements.php">
                 <span class="menu-title">Add Data</span>
                 <i class="mdi mdi-format-list-bulleted menu-icon"></i>
-              </a>
+              </a> 
             </li>
             <li class="nav-item sidebar-actions">
               <span class="nav-link">
